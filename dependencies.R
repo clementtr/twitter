afec2780ee4a4c922ba96d6092843f238fe6263d
clementT <clement.tailleur@gmail.com>
2017-02-06 15:58:12 +0100

    Add dependency RSentiment for english analysis

--- dependencies.R
+++ dependencies.R
@@ -1,3 +1,5 @@
-install.packages("twitteR", "tm", "wordcloud", "SnowballC", "plyr")
+install.packages("twitteR", "tm", "wordcloud", "SnowballC", "plyr", "RSentiment")
 require(devtools)
 install_github("lchiffon/wordcloud2")
+
+#RSentiment required Java
